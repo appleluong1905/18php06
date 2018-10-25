4020930caf474c9a0faa21ecafff78cf51421054
Chad Luong <apple.luong1905@gmail.com>
2018-10-25 20:41:53 +0700

    javascript

---
+++ session6/js/code_javascript.js
@@ -0,0 +1,47 @@
+//alert('test 1');
+document.write('Hello');
+document.write('<h1> Javascript</h1>');
+document.getElementById('demo').innerHTML = '<b>Hello PHP06</b>';
+console.log('Test javascript');
+/* 
+* comment 
+*/
+var a;
+// AZaz_09
+// Khong bat dau bang so
+// Camel case
+var user_name;
+var username;
+var UserName;
+var userName;
+// Co nghia
+// Tieng Anh
+
+/*
+	Khai bao 5 bien sau:
+	Ten, Tuoi, Nam sinh, Email, Que quan
+*/
+// Chua hoc C hoac pascal
+var a = 5;
+var email = 'abc@gmail.com';
+var b = 6;
+
+document.write('Tong a + b la:');
+document.write(a + b);
+document.write('<br>');
+document.write('Hieu a - b la:');
+document.write(a - b);
+
+// ham trong javascript
+function functionName() {
+	document.write('<br>Test function');
+}
+// goi ham
+functionName();
+function functionName2() {
+	var x = 5;
+	var y = 3;
+	return x + y;
+}
+document.write('<br>');
+document.write(functionName2());
