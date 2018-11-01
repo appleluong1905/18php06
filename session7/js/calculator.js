492fe7f5bd0092ce25b86465026f811e12bb473c
Chad Luong <apple.luong1905@gmail.com>
2018-11-01 19:08:11 +0700

    update cal

---
+++ session7/js/calculator.js
@@ -0,0 +1,11 @@
+var result = '';
+function addChar(char){
+	result+=char;
+	document.getElementById('input_field').innerHTML = result;
+}
+function resetCal(){
+	document.getElementById('input_field').innerHTML = 0;
+}
+function calculatorX(){
+	document.getElementById('input_field').innerHTML = eval(result);
+}
