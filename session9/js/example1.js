fd48de921930cc2ef46096c32d8aebd50db87b3f
Chad Luong <apple.luong1905@gmail.com>
2018-11-06 19:52:47 +0700

    update search

--- session9/js/example1.js
+++ session9/js/example1.js
@@ -1,14 +1,21 @@
 function searchText() {
 	var myName = document.getElementById('name').value;
 	var charReplace = document.getElementById('keyword').value;
+
+
+
 	var myNameNew = '';
 	var myNameLast = myName;
 	while(myName.indexOf(charReplace) != -1){
-		myNameNew = myNameNew + myName.slice(0, myName.indexOf(charReplace)) + '<span>' + charReplace + '</span>';
-		myName = myName.slice(myName.indexOf(charReplace) + charReplace.length, myName.length);
+		myNameNew = myNameNew + myName.slice(0, myName.indexOf(charReplace)) 
+		+ '<span>' + charReplace + '</span>';
+		myName = myName.slice(myName.indexOf(charReplace) 
+			+ charReplace.length, myName.length);
 	}
-	if((myNameLast.lastIndexOf(charReplace) != -1) && (myNameLast.lastIndexOf(charReplace) != myNameLast.length-1)){
-		myNameNew = myNameNew + myNameLast.slice(myNameLast.lastIndexOf(charReplace) + charReplace.length, myNameLast.length);
+	if((myNameLast.lastIndexOf(charReplace) != -1) 
+		&& (myNameLast.lastIndexOf(charReplace) != myNameLast.length-1)){
+		myNameNew = myNameNew + myNameLast.slice(myNameLast.lastIndexOf(charReplace) 
+			+ charReplace.length, myNameLast.length);
 
 	}
 	document.getElementById('demo').innerHTML = (myNameNew != '')?myNameNew:myName;
